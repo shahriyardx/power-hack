35d62d50e97d9cbc9e14306e46cdaad4b571883f
shahriyardx <mdshahriyaralam552@gmail.com>
2023-01-29 00:29:02 +0600

    Added login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Registration from "./pages/Registration";
+import Login from "./pages/Login";
 
 function App() {
   return <BrowserRouter>
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path="/registration" element={<Registration />} />
     </Routes>
   </BrowserRouter>
