043b79e3f74e5f2dfe2a78550c2145061afe9971
shahriyardx <mdshahriyaralam552@gmail.com>
2023-01-28 23:39:00 +0600

    Added register and login links to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from "react"
+import { NavLink } from "react-router-dom"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="bg-zinc-900 text-zinc-300">
+      <div className="container mx-auto p-5 flex items-center">
+        <h3 className="text-2xl font-bold">Power Hack</h3>
+        <div className="ml-auto">
+          <div className="flex items-center gap-5 text-xl">
+            <NavLink to="/login" className={({ isActive }) => isActive ? 'text-indigo-400 underline underline-offset-2' : ''}>Login</NavLink>
+            <NavLink to="/registration" className={({ isActive }) => isActive ? 'text-indigo-400 underline underline-offset-2' : ''}>Registration</NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
